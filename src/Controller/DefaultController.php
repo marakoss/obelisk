dc222e8f80b72eff564ff925a05fda80002792b7
Marek Králík <marakoss@gmail.com>
2020-02-03 23:38:42 +0100

    Introduced simple form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,12 +7,28 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController
 {
 
-    public function index()
-    {
-        $number = random_int(0, 10);
-
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
-    }
+	public function index()
+	{
+		return new Response(
+			'<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<form action="/upload" method="post" enctype="multipart/form-data">
+		
+				<input type="text" name="name" />
+				<input type="text" name="lat" />
+				<input type="text" name="lon" />
+				<input type="file" name="photo" />
+				<input type="submit" value="Upload" name="submit" />
+			</form>
+		</div>
+	</div>
+</body>
+</html>'
+		);
+	}
 }
